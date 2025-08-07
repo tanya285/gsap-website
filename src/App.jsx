b865ae2456874f54baee6817508b4f092535cc57
Tanya Bajwa <tanyabajwa@example.com>
2025-08-07 15:07:38 +0530

    Added parallax animation to cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Cocktails from "./components/Cocktails.jsx";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Cocktails/>
       <div className="h-dvh bg-black"></div>
     </main>
   );
